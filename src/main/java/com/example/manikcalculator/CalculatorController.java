75a935e888bdbc4b19f7dec38142cd72508dc7d0
varun <varun@gmail.com>
2023-04-17 15:44:58 +0800

    pom.xml

--- src/main/java/com/example/manikcalculator/CalculatorController.java
+++ src/main/java/com/example/manikcalculator/CalculatorController.java
@@ -1,37 +1,48 @@
 package com.example.manikcalculator;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class CalculatorController {
-
-    @GetMapping("/calculate")
-    @ResponseBody
-    public String calculate(@RequestParam("num1") int num1,
-                            @RequestParam("num2") int num2,
-                            @RequestParam("operation") String operation) {
-
-        int result = 0;
-
-        switch (operation) {
-            case "add":
-                result = num1 + num2;
-                break;
-            case "subtract":
-                result = num1 - num2;
-                break;
-            case "multiply":
-                result = num1 * num2;
-                break;
-            case "divide":
-                result = num1 / num2;
-                break;
+    
+    @RequestMapping(value="/calculate", method=RequestMethod.GET)
+    public ModelAndView calculate(@RequestParam("num1") String num1, @RequestParam("num2") String num2, @RequestParam("operation") String operation) {
+        double result = 0.0;
+        
+        try {
+            double n1 = Double.parseDouble(num1);
+            double n2 = Double.parseDouble(num2);
+            
+            switch (operation) {
+                case "add":
+                    result = n1 + n2;
+                    break;
+                case "subtract":
+                    result = n1 - n2;
+                    break;
+                case "multiply":
+                    result = n1 * n2;
+                    break;
+                case "divide":
+                    result = n1 / n2;
+                    break;
+                default:
+                    throw new IllegalArgumentException("Invalid operation: " + operation);
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Invalid number format: " + e.getMessage());
         }
-
-        return "Result: " + result;
+        
+        ModelAndView modelAndView = new ModelAndView("result");
+        modelAndView.addObject("num1", num1);
+        modelAndView.addObject("num2", num2);
+        modelAndView.addObject("operation", operation);
+        modelAndView.addObject("result", result);
+        
+        return modelAndView;
     }
 }
-
